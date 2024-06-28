9c79900254a15b171f641ea41dd2b273728ed91e
かりこ <webcreate0303yokoi0630@gmail.com>
2024-06-28 19:08:34 +0900

    dep :package: KumaUIの導入と設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// ---- KumaUI ----
+import KumaUI from "@kuma-ui/vite";
+
 // tsconfigを変更すると自動的にvite.configを設定してくれる
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react(), tsconfigPaths(), KumaUI({
+		// Enable WebAssembly support for Kuma UI. Default is false and will use Babel to transpile the code.
+		wasm: true // Optional
+	}),],
 	test: {
 		globals: true, // 作成したテストファイルで毎回メソッドのインポートをしなくて良くなる
 		environment: 'happy-dom', // 早い
