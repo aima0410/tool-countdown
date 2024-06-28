8d645b7efeb353ba008a8cc46007ffcd9d65c670
かりこ <webcreate0303yokoi0630@gmail.com>
2024-06-28 20:56:06 +0900

    feat :sparkles: イベントハンドラ設置、inputとpのトグル、自動フォーカスも

--- src/components/ui-elements/ContdownDisplay.tsx
+++ src/components/ui-elements/ContdownDisplay.tsx
@@ -1,9 +1,21 @@
+import { useState, useRef, useEffect } from "react";
+
 export default function CountdownDisplay() {
-  let showInput = false;
+  const [showInput, setShowInput] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // 自動フォーカスするための設定
+    if (showInput && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [showInput]);
+
   return (
     <>
       {showInput ? (
         <input
+          ref={inputRef}
           placeholder="00:00"
           value="00:00"
           type="text"
@@ -11,10 +23,19 @@ export default function CountdownDisplay() {
           maxLength={5}
           inputMode="numeric"
           onChange={() => {}}
+          onBlur={() => {
+            setShowInput(false);
+          }}
           name="countdown"
         />
       ) : (
-        <p>00:00</p>
+        <p
+          onClick={() => {
+            setShowInput(true);
+          }}
+        >
+          00:00
+        </p>
       )}
     </>
   );
