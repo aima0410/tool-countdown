d443fe343d32d7cbf4fbee4651223379558a6fb9
かりこ <webcreate0303yokoi0630@gmail.com>
2024-06-28 20:44:04 +0900

    style :lipstick: 真偽値でタイマーの表示を切り替え（<input> or <p>）

--- src/components/ui-elements/ContdownDisplay.tsx
+++ src/components/ui-elements/ContdownDisplay.tsx
@@ -1,14 +1,21 @@
 export default function CountdownDisplay() {
+  let showInput = false;
   return (
-    <input
-      placeholder="00:00"
-      value="00:00"
-      type="text"
-      pattern="[0-9:]*"
-      maxLength={5}
-      inputMode="numeric"
-      onChange={() => {}}
-      name="countdown"
-    />
+    <>
+      {showInput ? (
+        <input
+          placeholder="00:00"
+          value="00:00"
+          type="text"
+          pattern="[0-9:]*"
+          maxLength={5}
+          inputMode="numeric"
+          onChange={() => {}}
+          name="countdown"
+        />
+      ) : (
+        <p>00:00</p>
+      )}
+    </>
   );
 }
