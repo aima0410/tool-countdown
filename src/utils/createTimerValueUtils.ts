41343cae07bdf565502cae60336feda7604b6dfa
かりこ <webcreate0303yokoi0630@gmail.com>
2024-06-29 13:08:27 +0900

    refactor :recycle: 共通処理の抽出→createTimerValue()

--- src/utils/createTimerValueUtils.ts
+++ src/utils/createTimerValueUtils.ts
@@ -24,43 +24,38 @@ export function createTimerValueFromInput({ e, inputRef, timer }: Props) {
 
 	/* 01_タイマーの値を作成してセットする */
 	{
-		// タイマーからコロンを除去、新しい文字列として新規作成
-		const valueWithoutColon = timer.replace(':', '');
-		// 配列化
-		const array: Array<string> = valueWithoutColon.split('');
-
-		// Backspaceだった場合は
-		if (e.key === 'Backspace') {
-			// 01_最後の１文字を削除
-			array.pop();
-			// 02_先頭に"0"を追加
-			array.unshift('0');
-			// 03_コロンを追加
-			array.splice(2, 0, ':');
-		} else {
-			// 00_入力値の受け取り
-			const newValue = e.key;
-			// 01_最初の１文字目を削除
-			array.shift();
-			// 02_末尾に「入力値（value）」を追加
-			array.push(newValue);
-			// 03_コロンを追加
-			array.splice(2, 0, ':');
-		}
-		// 配列を文字列に戻す
-		const newTimerValue = array.join('');
-		// 新しいタイマーをリターン
+		// keyを取得
+		const key = e.key;
+		// keyが「Backspace」だった場合はdeleteする
+		const isDelete = key === 'Backspace';
+		const prevTimerValue = timer;
+		const newTimerValue = createTimerValue({ prevTimerValue, key, isDelete });
 		return newTimerValue;
 	}
 }
 
 export function createTimerValueFromNumPad(value: string, key: string) {
-	// タイマーからコロンを除去、新しい文字列として新規作成
-	const valueWithoutColon = value.replace(':', '');
+	// keyが「C」だった場合はdeleteする
+	const isDelete = key === 'C';
+	const prevTimerValue = value;
+	const newTimerValue = createTimerValue({ prevTimerValue, key, isDelete });
+	return newTimerValue;
+}
+
+// =================== このファイル内でのみ使用する関数 ===================
+interface TimerUpdatePorps {
+	prevTimerValue: string;
+	key: string;
+	isDelete: boolean;
+}
+
+function createTimerValue({ prevTimerValue, key, isDelete }: TimerUpdatePorps) {
+	// 前のタイマーからコロンを除去、新しい文字列として新規作成
+	const valueWithoutColon = prevTimerValue.replace(':', '');
 	// 配列化
 	const array: Array<string> = valueWithoutColon.split('');
 
-	if (key === 'C') {
+	if (isDelete) {
 		// 01_最後の１文字を削除
 		array.pop();
 		// 02_先頭に"0"を追加
