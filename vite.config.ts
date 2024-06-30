ad91e9b8c1bc16b14f262bfd90b1bd00601a5723
かりこ <webcreate0303yokoi0630@gmail.com>
2024-06-30 22:49:36 +0900

    docs :memo: GitHub Pagesへの自動デプロイの設定

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/tool-countdown/',
 	plugins: [
 		react(),
 		tsconfigPaths(),
